b4d9a757a96b699ba9e4b0a2947593e67225da8f
ronkips <kipshillash@gmail.com>
2022-11-12 18:05:34 +0300

    created the client, components folder and update the app.js

---
+++ client/src/components/PhoneVerify.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PhoneVerify = () => {
+  return (
+    <div>PhoneVerify</div>
+  )
+}
+
+export default PhoneVerify
--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-// Setting a simple Node.js server
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -7,3 +6,11 @@ const PORT = 4000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello world" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
