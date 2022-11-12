04f192e54bc4793a251c271a5c352845a13a498f
ronkips <kipshillash@gmail.com>
2022-11-12 14:47:17 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+// Setting a simple Node.js server
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
