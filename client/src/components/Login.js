2aebe8f79a4c879ac8f2e6dbefcdd407f3995de9
ronkips <kipshillash@gmail.com>
2022-11-13 14:52:27 +0300

    Created  the login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ email, password });
+    setPassword("");
+    setEmail("");
+  };
+
+  const gotoSignUpPage = () => navigate("/register");
+  return (
+    <div className="login__container">
+      <h2>Login</h2>
+      <form className="login__form" onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={email}
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={email}
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button className="loginBtn">SIGN IN</button>
+        <p>
+          Don't have an account?{""}
+          <span className="link" onClick={gotoSignUpPage}>
+            Sign up
+          </span>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
