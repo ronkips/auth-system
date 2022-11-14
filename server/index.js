b4dc401b079d0207b46685299274b51f62aeb681
ronkips <kipshillash@gmail.com>
2022-11-14 12:09:00 +0300

    updated the server to detect if the user exist when registering

--- server/index.js
+++ server/index.js
@@ -19,3 +19,31 @@ app.post("/api/register", (req, res) => {
   //Logs the crentedials to the console
   console.log({ email, password, tel, username });
 });
+
+// An array containing all the users
+const users = [];
+// Generate the random string as the ID
+const generateID = () => Math.random().toString(36).substring(2, 10);
+
+app.post("/api/regiter", (req, res) => {
+  // Get the user's credentials
+  const { email, password, tel, username } = req.body;
+
+  // Check if there is an existing user with the same email or password
+  let result = users.filter((user) => user.email === email || user.tel === tel);
+  // if none
+  if (result.length === 0) {
+    // create the structure for the user
+    const newUser = { id: generateID(), email, password, username, tel };
+    //Add user to the array of users
+    users.push(newUser);
+    // Returns a message
+    return res.json({
+      message: "Account created successfully"
+    });
+  }
+  // Returns the user exists
+  res.json({
+    error_message: "User already exists"
+  });
+});
